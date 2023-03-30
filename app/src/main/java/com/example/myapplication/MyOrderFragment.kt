8e06d5a72ff71ce731ee4bb636d7b85fce3ffc34
PC671 <mubinashukrullayeva@gmail.com>
2023-03-30 13:25:10 +0500

    fr

--- app/src/main/java/com/example/myapplication/MyOrderFragment.kt
+++ app/src/main/java/com/example/myapplication/MyOrderFragment.kt
@@ -1,21 +1,17 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentMyOrderBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MyOrderFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MyOrderFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -29,12 +25,19 @@ class MyOrderFragment : Fragment() {
         }
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_order, container, false)
+        val binding = FragmentMyOrderBinding.inflate(inflater, container, false)
+        val order = MyOrderFragment()
+        val name = param1
+        val password = param2
+        binding.user.text = name + " " + password
+
+
+        return binding.root
     }
 
     companion object {
